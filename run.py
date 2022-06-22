c0766328ec86591a97403fa723f20176ab3c3b4e
KeithBautista <keith1829@gmail.com>
2022-06-22 18:20:43 +0000

    Added in further init global var

--- run.py
+++ run.py
@@ -2,12 +2,31 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-# Global Variable for Grid as a whole
+"""
+---GLOBAL VARIABLES--
+"""
+
+# Grid as a whole
 grid = [[]]
 
-# Global Variable for Grid Size, This is to be manipulated based on input of user
+# Grid Size, To be manipulated based on input of user
 grid_size = 10
 
-# Global Variable for the amount of Ships user can place on Grid
+# the amount of Ships user can place on Grid
 num_of_ships = 8
+
+# Bullets Left
+bullets_left = 50
+
+# Game Over
+game_over = False
+
+# number of ships sunk
+num_of_ships_sunk = 0
+
+# Ship Positions
+ship_positions = [[]]
+
+# Alphabet to be used on the x axis of the board
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
