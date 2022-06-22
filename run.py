b79510cc6c57499ec43ce8ba5ab1d77e6e5f18f0
KeithBautista <keith1829@gmail.com>
2022-06-22 18:14:30 +0000

    Added in init var for grid and grid size

--- run.py
+++ run.py
@@ -2,4 +2,12 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-test = print(input("Type in your name here"))
+# Global Variable for Grid as a whole
+grid = [[]]
+
+# Global Variable for Grid Size, This is to be manipulated based on input of user
+grid_size = 10
+
+# Global Variable for the amount of Ships user can place on Grid
+num_of_ships = 8
+
